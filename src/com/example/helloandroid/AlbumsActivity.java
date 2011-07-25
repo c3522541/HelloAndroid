2b21cfc12155874c18c1952e9e89cc5c8683be91
sean <seangogo@gmail.com>
2011-07-25 17:57:47 -0400

    first commit

---
+++ src/com/example/helloandroid/AlbumsActivity.java
@@ -0,0 +1,19 @@
+package com.example.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AlbumsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		TextView textview = new TextView(this);
+		textview.setText("This is the Albums tab");
+		setContentView(textview);
+	}
+
+}
